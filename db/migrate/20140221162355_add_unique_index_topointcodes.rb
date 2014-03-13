8116960fd782cf8ddf0d03554ae0e0f3762d8e83
dominic.wong <166706916@qq.com>
2014-03-13 20:34:26 +0800

    去掉了pointcodes裏面的index

--- db/migrate/20140221162355_add_unique_index_topointcodes.rb
+++
@@ -1,9 +0,0 @@
-class AddUniqueIndexTopointcodes < ActiveRecord::Migration
-  def self.up
-    add_index :pointcodes, :secretcode, :uinque=>true
-  end
-
-  def self.down
-    remove_index :pointcodes, :secretcode
-  end
-end
