f3505222bbb5090b0a7b0c921678cbc0cee522d0
dominic.wong <166706916@qq.com>
2014-03-13 20:46:46 +0800

    增加了在shop中手動增加token的界面

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,5 @@
 class Shop < ActiveRecord::Base
-  attr_accessible :address, :agency, :cardtemplate, :logo, :name, :usertemplate_id, :phone, :exprieddate
+  attr_accessible :address, :agency, :cardtemplate, :logo, :name, :usertemplate_id, :phone, :exprieddate, :weixin_token
   belongs_to :usertemplate
   belongs_to :user
   self.per_page = 5
